04c5b3f96147107f332825a774ed882ca2f14198
kuliahumy26-byte <kuliahumy26@gmail.com>
2026-04-23 11:11:06 +0700

    menambahkan node baru

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -22,6 +22,13 @@ public:
 
     void addNode()
     {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa :";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
         
     }
 }

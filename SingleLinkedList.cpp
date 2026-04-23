cb40e250b463496236ab7292758d795f30f592d4
kuliahumy26-byte <kuliahumy26@gmail.com>
2026-04-23 11:43:52 +0700

    menambahkan void traverse

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -64,7 +64,7 @@ public:
         return (START == NULL);
     }
 
-    bool search(int ni,, Node *&previous, Node *&current)
+    bool search(int nim, Node *&previous, Node *&current)
     {
         previous = START;
         current = START;
@@ -74,6 +74,32 @@ public:
             previous = current;
             current = current->next;
         }
+
         return (current != NULL);
     }
-}
+
+    bool delNode(int nim)
+    {
+        Node *previous, *current;
+
+        if (!search(nim, previous, current))
+            return false;
+        
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+        
+        delete current;
+        return true;
+    }
+
+    void traverse()
+    {
+        if(ListEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+    }
+};
